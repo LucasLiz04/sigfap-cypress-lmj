ccc69f03f0be0ae25d6ed1b71fd0b1372a410b60
LucasLiz <lucasliz040203@gmail.com>
2026-06-15 23:04:04 -0400

    test: add cypress flows for profile and proposal

--- cypress/index.ts
+++ cypress/index.ts
@@ -1,5 +1,15 @@
 declare namespace Cypress {
   interface Chainable {
     typeLogin: (email: string, password: string) => void;
+    byCy: (
+      selector: string,
+      options?: Partial<Loggable & Timeoutable & Withinable & Shadow>,
+    ) => Chainable<JQuery<HTMLElement>>;
+    loginSigfap: () => void;
+    openUserProfile: () => void;
+    fillByCy: (selector: string, value: string | number) => void;
+    selectByCyOption: (selector: string, optionLabel: string) => void;
+    selectFirstByCyOption: (selector: string) => void;
+    saveCurrentStep: () => void;
   }
 }
